26e8d0138a4af1f619374658822f11b9d8858c47
Melike2 <melikesoygul@hotmail.com>
2023-08-16 10:18:08 +0300

    scroll categories

--- Landmarks/Views/Categories/CategoryRow.swift
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -10,13 +10,18 @@ struct CategoryRow: View {
         VStack(alignment: .leading) {
             Text(categoryName)
                 .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
 
 
-            HStack(alignment: .top, spacing: 0) {
-                ForEach(items) { landmark in
-                    Text(landmark.name)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
                 }
             }
+            .frame(height: 185)
         }
     }
 }
@@ -29,7 +34,7 @@ struct CategoryRow_Previews: PreviewProvider {
     static var previews: some View {
         CategoryRow(
             categoryName: landmarks[0].category.rawValue,
-            items: Array(landmarks.prefix(3))
+            items: Array(landmarks.prefix(4))
         )
     }
 }
