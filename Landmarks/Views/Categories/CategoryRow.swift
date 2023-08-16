e9f17cfa014e012b3d1d8999ec7c25eff33d0f68
Melike2 <melikesoygul@hotmail.com>
2023-08-16 10:08:16 +0300

    new

--- Landmarks/Views/Categories/CategoryRow.swift
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -1,20 +1,35 @@
-//
-//  CategoryRow.swift
-//  Landmarks
-//
-//  Created by Melike on 4.08.2023.
-//
-
 import SwiftUI
 
+
 struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+
+
+            HStack(alignment: .top, spacing: 0) {
+                ForEach(items) { landmark in
+                    Text(landmark.name)
+                }
+            }
+        }
     }
 }
 
+
 struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+
+
     static var previews: some View {
-        CategoryRow()
+        CategoryRow(
+            categoryName: landmarks[0].category.rawValue,
+            items: Array(landmarks.prefix(3))
+        )
     }
 }
