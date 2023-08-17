f8256552bcbc7fe0c1b136fbd42298f9cbbf579f
Melike2 <melikesoygul@hotmail.com>
2023-08-17 09:52:37 +0300

    profile changes

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @EnvironmentObject var modelData: ModelData
+    @Environment(\.editMode) var editMode
     @State private var draftProfile = Profile.default
 
 
     var body: some View {
-        Text("Profile for: \(draftProfile.username)")
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                if editMode?.wrappedValue == .active {
+                                   Button("Cancel", role: .cancel) {
+                                       draftProfile = modelData.profile
+                                       editMode?.animation().wrappedValue = .inactive
+                                   }
+                               }
+                Spacer()
+                EditButton()
+            }
+           
+            if editMode?.wrappedValue == .inactive {
+                            ProfileSummary(profile: modelData.profile)
+                        } else {
+                            ProfileEditor(profile: $draftProfile)
+                                .onAppear {
+                               draftProfile = modelData.profile
+                                                    }
+                               .onDisappear {
+                            modelData.profile = draftProfile
+                                                    }
+                        }
+        }
+                .padding()
     }
 }
 
@@ -20,5 +46,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
